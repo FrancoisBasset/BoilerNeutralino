f7d99dcb3cdf7215f2126c38a2fc98af7de747f9
FrancoisBasset <mrfrancoisbasset@gmail.com>
2025-07-22 11:51:54 +0200

    btgsb

--- vue-src/src/views/HomeView.vue
+++ vue-src/src/views/HomeView.vue
@@ -1,3 +1,17 @@
 <template>
-  <div>ok</div>
+  <div style="display: flex; flex-direction: column">
+    <button @click="counter++">Ajouter 1</button>
+    <button @click="counter += 2">Ajouter 2</button>
+    <button @click="counter += 5">Ajouter 5</button>
+    <button @click="counter += 10">Ajouter 10</button>
+    {{ counter }}
+  </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    counter: 0
+  })
+}
+</script>
